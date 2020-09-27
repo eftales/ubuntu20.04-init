d89feca7faea644f562b202521feac960581ea1d
eftales <1479803084@qq.com>
2020-09-27 19:13:00 +0800

    update:自带项和安装输入法的提示

--- ubuntu20.04-init.sh
+++ ubuntu20.04-init.sh
@@ -1,5 +1,13 @@
+# 已有项
+echo `
+20.04 自带：
+python3.8
+
+`
+
+
 # 安装中文输入法
-## 安装中文输入法首先需要在安装系统的时候选择中文版安装，否则就需要手动装中文语言包了。。
+echo "安装中文输入法首先需要在安装系统的时候选择中文版安装，否则就需要手动装中文语言包"
 
 echo `
 安装中文输入法请参考：
@@ -13,3 +21,12 @@ sudo apt install git -y
 # 安装 nv 驱动
 echo "参考：https://blog.csdn.net/z_6_2_0_s/article/details/106201929"
 
+# 安装 vscode 
+## 安装依赖
+sudo apt install software-properties-common apt-transport-https wget -y 
+
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo update
+sudo apt install code -y
+echo "记得安装 vs code 中文插件和 github theme"
